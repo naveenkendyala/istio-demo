081d63142481debff951e6ff63b86c1d902b559f
Naveen Kendyala <subscribeken@gmail.com>
2020-07-30 18:43:31 -0400

    draft quarkus application

---
+++ recommendation/java/quarkus/src/main/java/com/demo/ToDoResource.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/todos")
+public class ToDoResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}

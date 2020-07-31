ea1a101843d719866f580516f40ec22aa97b6e33
Naveen Kendyala <subscribeken@gmail.com>
2020-07-30 21:20:33 -0400

    updated docker file for v2

--- recommendation/java/quarkus/build-native.sh
+++ recommendation/java/quarkus/build-native.sh
@@ -2,6 +2,8 @@
 ./mvnw clean package -Pnative -Dquarkus.profile=native -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
 
 echo "*****************************************************************************"
+docker rmi istio-demo/recommendation:$1
+docker rmi quay.io/naveenkendyala/istio-demo-recommendation:$1
 docker build -f src/main/build/docker/Dockerfile.jvm -t istio-demo/recommendation:$1 .; 
 
 

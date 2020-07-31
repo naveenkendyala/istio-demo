37f12882195b89af10589119fe85f9e212bdc856
Naveen Kendyala <subscribeken@gmail.com>
2020-07-31 15:50:43 -0400

    Added customer project draft

---
+++ customer/java/quarkus/src/test/java/com/demo/NativeCustomerDoResourceIT.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCustomerDoResourceIT extends CustomerDoResourceTest {
+
+    // Execute the same tests but in native mode.
+}

a1818abccfc4bb06797ecb5bbdd762a3b92ee904
Naveen Kendyala <subscribeken@gmail.com>
2020-07-31 15:25:08 -0400

    Added istio control plane files

--- 00.demo.setup.sh
+++ 00.demo.setup.sh
@@ -3,17 +3,24 @@
 ## Must have cluster admin privileges
 ## Select the installation to be stable channel and in all “namespaces"
   # Elastic Search 	: Red Hat
-  # Jaeger 		    : Red Hat
-  # Kiali 			: Red Hat
-  # Service Mesh	: Red Hat
+  # Jaeger 		      : Red Hat
+  # Kiali 			    : Red Hat
+  # Service Mesh	  : Red Hat
 
 
 ## 02. Install Control Plane in a separate project
   # https://docs.openshift.com/container-platform/4.3/service_mesh/service_mesh_install/preparing-ossm-installation.html
-  # oc new-project istio-system
-  # oc create -n istio-system -f 01.istio-installation.yaml
+  # create istio-system project
+  oc new-project istio-system
+
+  # Install the insto-default 
+  oc create -n istio-system -f 01.istio-installation.yaml
+
+  # For customization; refer to the below
   # https://docs.openshift.com/container-platform/4.3/service_mesh/service_mesh_install/customizing-installation-ossm.html
-  # oc get smcp -n istio-system
+
+  # Check on the service mesh control plane
+  oc get smcp -n istio-system
 
 
 ## 03. Add your projects to the Control Plane : ServiceMeshMemberRoll
@@ -23,8 +30,11 @@
   # One project can belong to only one ServiceMeshMemberRoll
   # Log Into the namespace where the ServiceMeshControlPlane is installed [Ex: Istio-system]
   # Create ServiceMeshMemberRoll named “default” in same project as “ServiceMeshControlPlane”
-  # oc create -n istio-system -f 02.servicemeshmemberroll-default.yaml
+  oc create -n istio-system -f 02.servicemeshmemberroll-default.yaml
 
 ## Create tutorial project & allow pods to be run as root
-  # oc new-project tutorial
-  # oc adm policy add-scc-to-user anyuid -z default -n tutorial
+  # Create isttio-demo project
+  oc new-project istio-demo
+
+  # Override (if needed) to run containers as root
+  # oc adm policy add-scc-to-user anyuid -z default -n istio-demo

a73ee0df146c9bc182b4dc16641fc9193f91982d
Naveen Kendyala <subscribeken@gmail.com>
2020-08-01 18:18:07 -0400

    removed warnings

--- preference/java/quarkus/src/main/java/com/demo/resource/PreferenceResource.java
+++ preference/java/quarkus/src/main/java/com/demo/resource/PreferenceResource.java
@@ -3,9 +3,7 @@ package com.demo.resource;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
--- preference/java/quarkus/src/test/java/com/demo/PreferenceResourceTest.java
+++ preference/java/quarkus/src/test/java/com/demo/PreferenceResourceTest.java
@@ -1,10 +1,10 @@
 package com.demo;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+// import static io.restassured.RestAssured.given;
+// import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class PreferenceResourceTest {

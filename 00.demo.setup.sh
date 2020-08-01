80ad62ece1684d812b5d1a3a5207f9184c8ba161
Naveen Kendyala <subscribeken@gmail.com>
2020-08-01 13:47:07 -0400

    Added additional customization to Customer Service

--- 00.demo.setup.sh
+++ 00.demo.setup.sh
@@ -1,3 +1,4 @@
+## ------------------------------------------------------------------------------------------------------------------------------------------------
 ## 01. Install Operators : From the Web Console
 ## Starting with Red Hat OpenShift Service Mesh 1.1.4 requires Elastic Search, Jaeger and Kiali Operators before installing Service Mesh Operator
 ## Must have cluster admin privileges
@@ -7,7 +8,7 @@
   # Kiali 			    : Red Hat
   # Service Mesh	  : Red Hat
 
-
+## ------------------------------------------------------------------------------------------------------------------------------------------------
 ## 02. Install Control Plane in a separate project
   # https://docs.openshift.com/container-platform/4.3/service_mesh/service_mesh_install/preparing-ossm-installation.html
   # create istio-system project
@@ -22,7 +23,7 @@
   # Check on the service mesh control plane
   oc get smcp -n istio-system
 
-
+## ------------------------------------------------------------------------------------------------------------------------------------------------
 ## 03. Add your projects to the Control Plane : ServiceMeshMemberRoll
 ## Option#01
   # Create a “ServiceMeshMember” in individual projects and reference that to the "ServiceMeshControlPlane"
@@ -31,10 +32,42 @@
   # Log Into the namespace where the ServiceMeshControlPlane is installed [Ex: Istio-system]
   # Create ServiceMeshMemberRoll named “default” in same project as “ServiceMeshControlPlane”
   oc create -n istio-system -f 02.servicemeshmemberroll-default.yaml
+## ------------------------------------------------------------------------------------------------------------------------------------------------
 
+## 04. Create demo project
 ## Create tutorial project & allow pods to be run as root
   # Create isttio-demo project
   oc new-project istio-demo
 
   # Override (if needed) to run containers as root
   # oc adm policy add-scc-to-user anyuid -z default -n istio-demo
+
+
+## ------------------------------------------------------------------------------------------------------------------------------------------------
+## Local Code Setup
+## Deploying Customer Application
+## Run Jaeger locally
+docker run -d --name jaeger \
+  -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
+  -p 5775:5775/udp \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 14268:14268 \
+  -p 14250:14250 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:1.18
+
+## Jaeger can be accessed in the below URL
+http://localhost:16686
+
+
+
+## ------------------------------------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ customer/java/quarkus/src/main/java/com/demo/resource/CustomerResource.java
@@ -0,0 +1,69 @@
+package com.demo.resource;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.demo.service.PreferenceService;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/")
+public class CustomerResource {
+
+    //Create the output foramat
+    private static final String RESPONSE_STRING_FORMAT = "Customer => %s\n";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    //Inject the Rest Client
+    @Inject
+    @RestClient
+    PreferenceService preferenceService;
+
+    //Inbound Method for our Get Process
+    @GET
+    @Path("/customer")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getCustomer() {
+        try {
+
+            //Get the Response From Preference Service Call
+            String response = preferenceService.getPreference().trim();
+
+            //Returning the Response From the Preferencxe Call
+            return Response.ok(String.format(RESPONSE_STRING_FORMAT, response)).build();
+
+        }catch(Exception pException){
+            
+            //Log the Exception Details
+            logger.warn("Exception trying to get the response from preference service.", pException);
+
+            //Format the Response to Include the Path and the Exception Details
+            return Response
+                    .status(Response.Status.SERVICE_UNAVAILABLE)
+                    .entity(String.format(RESPONSE_STRING_FORMAT,pException.getCause().getClass().getSimpleName()+" ["+pException.getCause().getMessage()+"]"))
+                    .build();
+        }
+
+        // } catch (WebApplicationException ex) {
+        //     Response response = ex.getResponse();
+        //     logger.warn("Non HTTP 20x trying to get the response from preference service: " + response.getStatus());
+        //     return Response.status(Response.Status.SERVICE_UNAVAILABLE)
+        //             .entity(String.format(RESPONSE_STRING_FORMAT,
+        //                     String.format("Error: %d - %s", response.getStatus(), response.readEntity(String.class))))
+        //             .build();
+        // } catch (ProcessingException ex) {
+        //     logger.warn("Exception trying to get the response from preference service.", ex);
+        //     return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(String.format(RESPONSE_STRING_FORMAT,
+        //             ex.getCause().getClass().getSimpleName() + ": " + ex.getCause().getMessage())).build();
+        // }
+    }
+}

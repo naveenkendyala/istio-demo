a9d9381a79a90b5e4b3672abc4675805e0a780a5
Naveen Kendyala <subscribeken@gmail.com>
2020-08-01 18:11:19 -0400

    Updated Exception Handling

--- preference/java/quarkus/src/main/java/com/demo/resource/PreferenceResource.java
+++ preference/java/quarkus/src/main/java/com/demo/resource/PreferenceResource.java
@@ -18,18 +18,35 @@ import org.slf4j.LoggerFactory;
 @Path("/")
 public class PreferenceResource {
 
+    private static final String RESPONSE_STRING_FORMAT = "Preference => %s\n";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
 
     @Inject
     @RestClient
     RecommendationService recommendationService;
 
-    //Inbound Method for our Get Process
+    // Inbound Method for our Get Process
     @GET
     @Path("/preference")
     @Produces(MediaType.TEXT_PLAIN)
-    public Response getCustomer() {        
-        return Response.ok(recommendationService.getRecommendation()).build();
+    public Response getPrefernce() {
 
+        try {
+            // Get the Reponse and Send
+            String response = recommendationService.getRecommendation();
+            return Response.ok(response).build();
+        } catch (Exception pException) {
+
+            //Log the Exception Details
+            logger.warn("Exception trying to get the response from Recommendation Service.", pException);
+
+
+            //Format the Response to Include the Path and the Exception Details
+            return Response
+                    .status(Response.Status.SERVICE_UNAVAILABLE)
+                    .entity(String.format(RESPONSE_STRING_FORMAT,pException.getCause().getClass().getSimpleName()+" ["+pException.getCause().getMessage()+"]"))
+                    .build();
+        }
     }
 }

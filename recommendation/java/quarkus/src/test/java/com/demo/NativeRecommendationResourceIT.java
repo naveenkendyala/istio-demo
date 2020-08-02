1045f6b98ab08584e9830261f454ca57b9eae398
Naveen Kendyala <subscribeken@gmail.com>
2020-08-02 18:29:20 -0400

    Cleanup recommendation quarkus

--- recommendation/java/quarkus/src/test/java/com/demo/NativeRecommendationResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.demo;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeRecommendationResourceIT extends RecommendationResourceTest {
-
-    // Execute the same tests but in native mode.
-}

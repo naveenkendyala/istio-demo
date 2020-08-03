c5a5a0f1d58bdab83c5dd0481f413f6a89f8479d
Naveen Kendyala <subscribeken@gmail.com>
2020-08-02 20:46:18 -0400

    Fixed issues with the recommendation and resyncing the draft

---
+++ recommendation/java/quarkus/src/test/java/com/demo/NativeRecResourceIT.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeRecResourceIT extends RecResourceTest {
+
+    // Execute the same tests but in native mode.
+}
